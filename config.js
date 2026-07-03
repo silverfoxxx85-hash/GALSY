f8761e00bc90eb9c00acd995d85afd8a8ab02607
silverfoxxx85-hash <silverfoxxx85@gmail.com>
2026-07-03 02:37:20 +0200

    config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+JavaScript
+// config.js - Predisposizione per l'integrazione Cloudinary (Release Future)
+const cloudConfig = {
+    cloudName: "", // Inserire qui il Cloud Name
+    apiKey: "",    // Inserire qui l'API Key
+    apiSecret: ""  // Inserire qui l'API Secret (se necessario per backend)
+};
